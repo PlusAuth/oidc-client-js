c7008dcac9a69a34a2c3d50f72ed8fbf53ac0ee8
ayazismailhakki <ayazismailhakki@gmail.com>
2020-10-08 16:45:04 +0300

    test: add in memory storage adapter tests

--- test/helpers/state_manager.spec.ts
+++ test/helpers/state_manager.spec.ts
@@ -1,6 +1,7 @@
-import {LocalStorageStateStore} from "../../src/helpers";
+import {InMemoryStateStore, LocalStorageStateStore} from "../../src/helpers";
 
 const store = new LocalStorageStateStore('test:')
+const inMemory = new InMemoryStateStore()
 
 describe('state manager', function () {
   describe('localStorage', function () {
@@ -61,6 +62,72 @@ describe('state manager', function () {
               const key = window.localStorage.key( i )
               // @ts-ignore
               const storedObj = JSON.parse(window.localStorage.getItem(key))
+              if(storedObj.created_at){
+                expect(storedObj.created_at).toBeGreaterThanOrEqual(7000)
+              }
+            }
+            done()
+          })
+        })
+    });
+  })
+
+  describe('inMemory', function () {
+    it('should set', function (done) {
+      const obj = { a: 1}
+      inMemory.set('x', obj).then(()=>{
+        expect(inMemory.map.get('x'))
+          .toBe(obj)
+        done()
+      })
+    });
+
+    it('should get', function (done) {
+      inMemory.get('notExisted').then((val)=>{
+        expect(val).toBeNull()
+        done()
+      })
+    });
+    it('should get (2)', function (done) {
+      inMemory.get('x').then((val)=>{
+        const obj = { a: 1}
+        expect(val).toStrictEqual(obj)
+        done()
+      })
+    });
+
+    it('should delete', function (done) {
+      inMemory.del('x').then(()=>{
+        inMemory.get('x').then(v => {
+          expect(v).toBeNull()
+          done()
+        })
+      })
+    });
+
+    it('should clear all', function (done) {
+      const objects = Array(10).fill({x: 'x'})
+      Promise.all(objects.map((obj, ind) => inMemory.set(String(ind), obj)))
+        .then(() => {
+          inMemory.clear().then(()=>{
+            expect(inMemory.map.size).toBe(0)
+            done()
+          })
+        })
+    });
+
+    it('should clear expired', function (done) {
+      const objects = []
+      inMemory.map.set('shouldNotDelete', '{ "a": "b"}')
+      for (let i = 1; i <= 10; i++) {
+        objects.push({ created_at: i * 1000 })
+      }
+      Promise.all(objects.map((obj, ind) => inMemory.set(String(ind), obj)))
+        .then(() => {
+          inMemory.clear(7000).then(()=>{
+            expect(inMemory.map.size).toBe(5)
+            for (const key of inMemory.map.keys()) {
+              const storedObj = inMemory.map.get(key)
               if(storedObj.created_at){
                 expect(storedObj.created_at).toBeGreaterThanOrEqual(7000)
               }
