5e40a00f7f5b257192f245b398042d8af748f843
ayazismailhakki <ayazismailhakki@gmail.com>
2020-10-08 10:59:38 +0300

    feat: in memory state store

---
+++ src/helpers/state_manager/in_memory.ts
@@ -0,0 +1,32 @@
+import { StateStore } from './state_store';
+
+export class InMemoryStateStore extends StateStore {
+  map = new Map()
+
+  clear( before?: number ) {
+    if ( before ){
+      this.map.forEach( ( val, ind ) => {
+        if ( val.created_at < before ){
+          this.map.delete( ind )
+        }
+      } )
+      return Promise.resolve()
+    } else {
+      return Promise.resolve( this.map.clear() );
+    }
+  }
+
+  del( key: string ) {
+    this.map.delete( key )
+    return Promise.resolve( );
+  }
+
+  get( key: string ) {
+    return Promise.resolve( this.map.get( key ) || null );
+  }
+
+  set( key: string, value: any ) {
+    this.map.set( key, value )
+    return Promise.resolve();
+  }
+}
